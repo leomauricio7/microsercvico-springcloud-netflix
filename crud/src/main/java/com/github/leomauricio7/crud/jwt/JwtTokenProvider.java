2a89923525c849a2af0955212ee916d2c5982036
Leonardo Mauricio <ter_leonardo.silva@tempoassist.com.br>
2021-06-24 13:56:05 -0300

    add spring security service:crud and service:pagamento

---
+++ crud/src/main/java/com/github/leomauricio7/crud/jwt/JwtTokenProvider.java
@@ -0,0 +1,96 @@
+package com.github.leomauricio7.crud.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Collection;
+import java.util.Date;
+
+@Service
+public class JwtTokenProvider {
+
+    @Value("${security.jwt.token.secret-key}")
+    private String secreteKey;
+
+    // metodo de criptografar a senha
+    @PostConstruct // executa no inicio para criptografar a senha
+    protected void init() {
+        secreteKey = Base64.getEncoder().encodeToString(secreteKey.getBytes());
+    }
+
+    // metodo de autenticação do token
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = new UserDetails() {
+
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                return null;
+            }
+
+            @Override
+            public String getPassword() {
+                return "";
+            }
+
+            @Override
+            public String getUsername() {
+                return "";
+            }
+
+            @Override
+            public boolean isAccountNonExpired() {
+                return true;
+            }
+
+            @Override
+            public boolean isAccountNonLocked() {
+                return true;
+            }
+
+            @Override
+            public boolean isCredentialsNonExpired() {
+                return true;
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return true;
+            }
+        };
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    // metodo de resolver token
+    public String resolveToken(HttpServletRequest httpServletRequest) {
+        String bearerToken = httpServletRequest.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer")) {
+            return bearerToken.substring(7, bearerToken.length());
+        }
+        return null;
+    }
+
+    // metodo de validar token
+    public boolean validaToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secreteKey).parseClaimsJws(token);
+            if (claims.getBody().getExpiration().before(new Date())) {
+                return false;
+            }
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+}
